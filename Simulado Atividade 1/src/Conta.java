9423187819dbe8b95a4cb4f400ac91b62e98e3bc
SocioPJ <20.00704-3@maua.br>
2022-04-15 12:29:14 -0300

    Corrigindo a classe Conta

--- Simulado Atividade 1/src/Conta.java
+++ Simulado Atividade 1/src/Conta.java
@@ -1,32 +1,62 @@
+
+import java.util.HashSet;
 public class Conta {
     //Atributos da classe
+    private static HashSet<Integer> idConta = new HashSet<>();
     private int idConta;
     private double saldo;
 
-    //Construtor:
-    public Conta(int idConta,double saldo){
+    //Construtor da Classe:
+
+    public Conta(double saldo){
+        int novoId = idConta.size()+1;
+        idConta.add(novoId);
         this.idConta = idConta;
         this.saldo = saldo;
     }
-    //Métodos da classe:
-    public void visualizarConta(){
-        System.out.println("ID da conta:" + idConta);
-        System.out.println("Saldo:" + saldo);
-    }
-    public int getidConta(){
+    
+    //Getter
+    // Retorna o id da conta
+    public int getIdConta(){
         return idConta;
     }
-
-    public void setNome(int idConta){
-        this.idConta = idConta;
-    }
-
+    //Retorna o saldo da conta
     public double getsaldo(){
         return saldo;
     }
 
-    public void setSaldo(double saldo){
-        this.saldo = saldo;
+//Métodos da classe:
+public void visualizarConta(){
+    System.out.println("ID da conta:" + idConta);
+    System.out.println("Saldo:" + saldo);
+}
+//Deposita um valor na conta
+public void depositar(double valor){
+    this.saldo += valor;
+}
+//Retira o valor do saldo se ele não for maior que o saldo
+public boolean sacar(double valor){
+    if(valor <= saldo){
+        this.saldo -= valor;
+        return true;
     }
+    return false;
+}
+//Transfere uma determinada quatiade de dinheiro para outra conta
+public boolean transferir(Conta contaDestino, double valor){
+    if(valor <= saldo){
+        this.saldo -= valor;
+        contaDestino.saldo += valor;
+        return true;
+    }
+    return false;
+}
+//Informacoes da conta
+public String toString(){
+    return "Contas{"+
+    "idConta="+idConta +
+    ", saldo="+saldo+
+    '}';
+}
 
 }
